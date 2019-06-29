2900caf154687f0edd7d0ee2b0fd23be6ddb64e2
shadman264 <shadman_264@yahoo.com>
2019-06-29 15:25:56 -0500

    sass foundation done

--- src/module/Album/Album.component.js
+++ src/module/Album/Album.component.js
@@ -2,47 +2,36 @@ import React, { Component } from 'react';
 import data from './Album.mock';
 import {Flex, Box} from 'reflexbox';
 
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 export default class Album extends Component {
 	constructor() {
-		super();
+    super();
+    this.handleThumbnailClick = this.handleThumbnailClick.bind(this);
 		this.state = {
-			albumData: data
+			albumData: data,
+			albumTable: []
 		}
-	}
+  }
+  
+  handleThumbnailClick(album) {
+    console.log('i came here');
+    console.log(album);
+  }
 
 	componentDidMount() {
-		// let albumTable = null;
-		// let albumRow = null;
-		// data.map((album, index) => {
-		// 	if(index !== 0 && index % 3 === 0) {
-		// 		albumTable.push(
-		// 			<Flex w={1}>
-		// 				{albumRow}
-		// 			</Flex>
-		// 		);
-		// 		albumRow = null;
-		// 	} else{
-		// 		albumRow.push(
-		// 			<Box w={1/3}>
-		// 				<img src={album.thumbnailUrl} alt="thumbnail"/>
-		// 			</Box>
-		// 		)
-		// 	}
-		// })
-	}
-
-	render() {
-		// const albumThumbnails = data.map(album => {
-		// 	return (
-		// 		<img src={album.thumbnailUrl} alt="thumbnail"/>
-		// 	)
-		// })
 		let albumTable = [];
-		let albumRow = [];
+    let albumRow = [];
 		data.map((album, index) => {
 			albumRow.push(
 				<Box>
-					<img src={album.thumbnailUrl} alt="thumbnail"/>
+					<img src={album.thumbnailUrl} alt="thumbnail" onClick={() => this.handleThumbnailClick(album)}/>
 				</Box>
 			)
 			if((index + 1) % 3 === 0) {
@@ -57,10 +46,27 @@ export default class Album extends Component {
 			}
 			return null;
 		});
-		return(
-			<div>
-				{albumTable}
-			</div>			
-		)
+		this.setState({
+			albumTable
+		})
+	}
+
+	render() {
+		// const albumThumbnails = data.map(album => {
+		// 	return (
+		// 		<img src={album.thumbnailUrl} alt="thumbnail"/>
+		// 	)
+		// })
+		
+		// return(
+		// 	<div>
+		// 		{this.state.albumTable}
+		// 	</div>			
+		// )
+	// const classes = useStyles;
+
+  return (
+    <h1 className="album media">hellow</h1>
+  );
 	}
 }
