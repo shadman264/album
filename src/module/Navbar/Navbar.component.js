16b509ff8e0d1f1bff3d1f9532d298528988f91e
shadman264 <shadman_264@yahoo.com>
2019-07-02 19:34:14 -0500

    added filter textField

--- src/module/Navbar/Navbar.component.js
+++ src/module/Navbar/Navbar.component.js
@@ -4,24 +4,42 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from '@material-ui/icons/FilterList';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 
 class Navbar extends Component{
   render() {
     return(
       <AppBar position="sticky">
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="Search">
+          <IconButton edge="start" color="inherit" aria-label="Menu">
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className="navbar-left">
             Albums
           </Typography>
+          <div>
+            <Grid container spacing={1} alignItems="flex-end">
+              <Grid item>
+                <TextField id="navbar-filter" />
+              </Grid>
+              <Grid item>
+                <SearchIcon />
+              </Grid>
+            </Grid>
+          </div>
+          {/* <TextField
+            id="standard-search"
+            label="Search"
+            type="search"
+            className="navbar-search"
+            margin="normal"
+          />
           <IconButton edge="start" color="inherit" aria-label="Search">
             <SearchIcon />
-          </IconButton>
+          </IconButton> */}
         </Toolbar>    
       </AppBar>
     )
